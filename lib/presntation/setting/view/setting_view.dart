c55597c21096a52f8e36cf831f699d563d3daff1
khaledgomah <khaledgomah10@gmail.com>
2025-02-05 00:30:22 +0200

    edit setting view

--- lib/presntation/setting/view/setting_view.dart
+++ lib/presntation/setting/view/setting_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:responsive/presntation/setting/view/widgets/save_button.dart';
 import 'package:responsive/presntation/setting/view/widgets/setting_selection.dart';
 
 class SettingView extends StatelessWidget {
@@ -9,11 +8,7 @@ class SettingView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.max,
-        children: [Expanded(child: SettingSelection()), SaveButton()],
-      ),
+      body: const Expanded(child: SettingSelection())
     );
   }
 }
