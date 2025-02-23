0bec331c2f8de55771ac87b70c2da693b0db02f2
khaledgomah <khaledgomah10@gmail.com>
2025-02-23 14:43:53 +0200

    fix pug

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:responsive/app/application_class.dart';
+import 'package:responsive/app/di.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,6 +13,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+   await initAppModule();
   runApp(ProviderScope(child: MyApp()));
 }
 
